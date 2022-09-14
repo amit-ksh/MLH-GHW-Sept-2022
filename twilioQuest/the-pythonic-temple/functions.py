41889dbc6495942065f446bfd34ab19809fa10a7
Amit Kumar Sharma <ksamit1110@gmail.com>
2022-09-14 11:33:09 +0530

    Update functions.py

--- twilioQuest/the-pythonic-temple/functions.py
+++ twilioQuest/the-pythonic-temple/functions.py
@@ -16,5 +16,3 @@ def add_numbers(a, b):
 
 
 print(add_numbers(5, 2))
-
-# {"body": "Hi there", "num_segments": "1", "direction": "outbound-api", "from": "+18145686198", "date_updated": "Tue, 13 Sep 2022 08:53:48 +0000", "price": null, "error_message": null, "uri": "/2010-04-01/Accounts/AC72e8ef1a3b2c39e6f7912b5aebe1dcc1/Messages/SM90a5b77e32dd514b71c23db283db2371.json", "account_sid": "AC72e8ef1a3b2c39e6f7912b5aebe1dcc1", "num_media": "0", "to": "+17078760365", "date_created": "Tue, 13 Sep 2022 08:53:48 +0000", "status": "queued", "sid": "SM90a5b77e32dd514b71c23db283db2371", "date_sent": null, "messaging_service_sid": null, "error_code": null, "price_unit": "USD", "api_version": "2010-04-01", "subresource_uris": {"media": "/2010-04-01/Accounts/AC72e8ef1a3b2c39e6f7912b5aebe1dcc1/Messages/SM90a5b77e32dd514b71c23db283db2371/Media.json"}}
